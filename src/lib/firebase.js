299abe65bd10759e6ac843b924b91a328c6de671
benomilo55 <benomilo55@gmail.com>
2026-08-15 16:19:28 +0300

    Initialize Firestore

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -12,4 +13,7 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig)
 
+const db = getFirestore(app)
+
 export default app
+export { db }
